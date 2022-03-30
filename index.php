e6366265e97056d975296a6ef541be221414f082
daemonprocellae@live.it <daemonprocellae@live.it>
2022-03-30 19:13:09 +0200

    Aggiunto header

--- index.php
+++ index.php
@@ -4,9 +4,41 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <title>Gooogle policiess</title>
 </head>
 <body>
-    
+    <header>
+        <div id="first-line">
+            <figure>
+                <img src="./img/logo.png">
+            </figure>
+
+            <div id="icon-wrapper">
+                <figure id="menu">
+                    <img src="./img/dots-menu.png">
+                </figure>
+
+                <figure id="pro-pic">
+                    <img src="https://i0.wp.com/newdoorfiji.com/wp-content/uploads/2018/03/profile-img-1.jpg?ssl=1">
+                </figure>
+            </div>
+        </div>
+
+        <nav>
+            <ul>
+                <li>Introduzione</li>
+                <li>Norme sulla privacy</li>
+                <li>Termini di servizio</li>
+                <li>Tecnologie</li>
+                <li class="active">Domande frequenti</li>
+            </ul>
+        </nav>
+    </header>
+
+    <main></main>
+
+    <footer></footer>
 </body>
 </html>
